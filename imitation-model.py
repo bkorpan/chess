3ef692ade7e296f969ee018648713347de3ecc6a
Ben Korpan <bkorpan@berkeley.edu>
2023-03-29 19:59:14 -0700

    Overhauled model code

--- imitation-model.py
+++ imitation-model.py
@@ -1,47 +1,13 @@
-import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import chess.pgn
+import random
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.optimizers import Adam
-from chess import pgn
-import chess
 from collections import defaultdict
-from typing import List, Tuple
-
-from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.pre_tokenizers import Whitespace
-from tokenizers.processors import TemplateProcessing
-from tokenizers.trainers import BpeTrainer
 
 # Pre-processing functions
-
-def create_tokenizer() -> Tokenizer:
-    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-    tokenizer.pre_tokenizer = Whitespace()
-    trainer = BpeTrainer(special_tokens=["[UNK]"], vocab_size=32000)
-    tokenizer.post_processor = TemplateProcessing(
-        single="[UNK] $A",
-        special_tokens=[("[UNK]", tokenizer.token_to_id("[UNK]"))],
-    )
-
-    tokenizer.train_from_iterator(generate_tokenizer_data(), trainer=trainer)
-    return tokenizer
-
-
-def generate_tokenizer_data():
-    for board in chess.Board().legal_boards():
-        yield encode_board(board)
-        for move in board.legal_moves:
-            yield move.uci()
-
-
-def encode_board(board: chess.Board) -> str:
-    return "\n".join(
-        "".join(board.piece_at(chess.square(rank, file)) for file in range(8))
-        for rank in range(7, -1, -1)
-    )
-
 
 def load_filtered_games(pgn_filepath):
     with open(pgn_filepath) as pgn_file:
@@ -53,165 +19,222 @@ def load_filtered_games(pgn_filepath):
             games.append(game)
     return games
 
-def parse_game(game: chess.pgn.Game) -> List[Tuple[chess.Board, chess.Move]]:
-    result = []
-    node = game
-    while not node.is_end():
-        board = node.board()
-        move = node.variations[0].move
-        result.append((board, move))
-        node = node.variations[0]
 
-    return result
+def tokenize_board(board):
+    tokenized_board = []
 
-def parse_game(game: chess.pgn.Game, player: str) -> List[Tuple[chess.Board, chess.Move]]:
-    moves = []
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+
+        if piece:
+            piece_str = piece.symbol()
+            if board.has_castling_rights(piece.color, chess.CASTLING_ROOK_MASK):
+                if piece.piece_type == chess.KING:
+                    piece_str = piece_str.upper() + "'"
+                elif piece.piece_type == chess.ROOK:
+                    piece_str = piece_str.upper() + "'"
+            if piece.piece_type == chess.PAWN and board.is_double_pawn_push(square):
+                piece_str = piece_str.upper() + "'"
+        else:
+            piece_str = '0'
+
+        tokenized_board.append(piece_str)
+
+    return tokenized_board
+
+
+def tokenize_move(move):
+    return chess.square_name(move.from_square) + chess.square_name(move.to_square) + move.promotion_uci_suffix()
+
+
+def game_to_tokenized_pairs(game):
+    tokenized_pairs = []
     board = game.board()
 
     for move in game.mainline_moves():
-        if board.turn == chess.WHITE and player == game.headers["White"]:
-            moves.append((board.copy(), move))
-        elif board.turn == chess.BLACK and player == game.headers["Black"]:
-            moves.append((board.copy(), move))
+        tokenized_board = tokenize_board(board)
+        tokenized_move = tokenize_move(move)
+        tokenized_pairs.append((tokenized_board, tokenized_move))
+
         board.push(move)
 
-    return moves
-
-# Model creation function
-def create_transformer_model(input_dim, hidden_dim, output_dim, num_heads, num_layers, dff):
-    inputs = Input(shape=(None,), dtype=tf.int32)
-    x = tf.keras.layers.Embedding(input_dim, hidden_dim)(inputs)
-
-    for _ in range(num_layers):
-        x = tf.keras.layers.MultiHeadAttention(num_heads, hidden_dim // num_heads)(x, x)
-        x = tf.keras.layers.LayerNormalization()(x)
-        x = tf.keras.layers.PositionWiseFeedForward(hidden_dim, dff)(x)
-        x = tf.keras.layers.LayerNormalization()(x)
-
-    x = Dense(output_dim, activation="softmax")(x)
-    return tf.keras.Model(inputs=inputs, outputs=x)
-
-# Pre-training functions
-
-def generate_pretraining_data(filtered_games):
-    pretraining_data = []
-
-    for game in filtered_games:
-        pretraining_data.extend(parse_game(game))
-
-    return pretraining_data
+    return tokenized_pairs
 
 
-def pretrain(model, pretraining_data, epochs, batch_size):
-    tokenizer = create_tokenizer()
-    input_dim = len(tokenizer.get_vocab())
+def prepare_pretraining_set(dataset):
+    pretraining_set = []
 
-    optimizer = Adam(learning_rate=1e-3)
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+    for game in dataset:
+        pretraining_set.extend(game_to_tokenized_pairs(game))
 
-    for epoch in range(epochs):
-        np.random.shuffle(pretraining_data)
-
-       	for i in range(0, len(pretraining_data), batch_size):
-            batch_data = pretraining_data[i:i + batch_size]
-
-            input_seqs = [tokenizer.tokenize(state).to_tensor() for state, move in batch_data]
-            input_seqs = tf.keras.preprocessing.sequence.pad_sequences(input_seqs, padding="post")
-            target_seqs = [tokenizer.tokenize(move.uci()).to_tensor() for state, move in batch_data]
-            target_seqs = tf.keras.preprocessing.sequence.pad_sequences(target_seqs, padding="post")
-
-            with tf.GradientTape() as tape:
-                predictions = model(input_seqs, training=True)
-                loss = loss_fn(target_seqs, predictions)
-
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-        print(f"Epoch {epoch + 1}/{epochs}: Loss = {loss.numpy()}")
-
-# Meta-training functions
-
-def inner_loop(model, support_set, inner_lr):
-    optimizer = Adam(learning_rate=inner_lr)
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
-
-    updated_weights = model.get_weights()
-
-    for state, move in support_set:
-        input_seq = tokenizer.tokenize(encode_board(state)).to_tensor()[tf.newaxis, :]
-        target_seq = tokenizer.tokenize(move.uci()).to_tensor()[tf.newaxis, :]
-
-        with tf.GradientTape() as tape:
-            predictions = model(input_seq, training=True)
-            loss = loss_fn(target_seq, predictions)
-
-        gradients = tape.gradient(loss, model.trainable_variables)
-        updated_weights = [w - inner_lr * g for w, g in zip(updated_weights, gradients)]
-
-    return updated_weights
+    random.shuffle(pretraining_set)
+    return pretraining_set
 
 
-def meta_train(model, games_by_player, epochs, k_shot_support, k_shot_query, meta_lr, inner_lr):
-    optimizer = Adam(learning_rate=meta_lr)
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+def prepare_metatraining_set(dataset):
+    metatraining_set = defaultdict(list)
 
-    for epoch in range(epochs):
-        player = np.random.choice(list(games_by_player.keys()))
+    for game in dataset:
+        white_player = game.headers['White']
+        black_player = game.headers['Black']
+        game_data = game_to_tokenized_pairs(game)
+        white_moves = game_data[0::2]
+        black_moves = game_data[1::2]
+        metatraining_set[white_player].extend(white_moves)
+        metatraining_set[black_player].extend(black_moves)
 
-        player_data = []
-        for game in games_by_player[player]:
-            player_moves = parse_game(game, player)
-            player_data.extend(player_moves)
-
-        np.random.shuffle(player_data)
-
-        support_set = player_data[:k_shot_support]
-        query_set = player_data[k_shot_support:k_shot_support + k_shot_query]
-
-        updated_weights = inner_loop(model, support_set, inner_lr)
-        model.set_weights(updated_weights)
-
-        input_seqs = [tokenizer.tokenize(state).to_tensor() for state, move in query_set]
-        input_seqs = tf.keras.preprocessing.sequence.pad_sequences(input_seqs, padding="post")
-        target_seqs = [tokenizer.tokenize(move.uci()).to_tensor() for state, move in query_set]
-        target_seqs = tf.keras.preprocessing.sequence.pad_sequences(target_seqs, padding="post")
-
-        with tf.GradientTape() as tape:
-            predictions = model(input_seqs, training=True)
-            loss = loss_fn(target_seqs, predictions)
-
-        gradients = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-        print(f"Meta-training Epoch {epoch + 1}/{epochs}: Loss = {loss.numpy()}")
+    return metatraining_set
 
 
-# Training the model
-script_dir = os.path.dirname(os.path.realpath(__file__))
-file_name = 'filtered_games.pgn'
-file_path = os.path.join(script_dir, file_name)
-filtered_games = load_filtered_games(file_path)
+# Model classes
 
-pretraining_data = generate_pretraining_data(filtered_games)
+# Define the encoding dictionary
+TOKENS = {
+    'P': 1, 'N': 2, 'B': 3, 'R': 4, 'Q': 5, 'K': 6,
+    'p': 7, 'n': 8, 'b': 9, 'r': 10, 'q': 11, 'k': 12,
+    'R\'': 13, 'K\'': 14, 'r\'': 15, 'k\'': 16,
+    'P\'': 17, 'p\'': 18, '0': 0
+}
 
-# Model parameters
-input_dim = len(tokenizer.get_vocab())
-hidden_dim = 128
-output_dim = len(tokenizer.get_vocab())
-num_heads = 4
-num_layers = 2
-dff = 512
+class GPTDecoderLayer(nn.TransformerDecoderLayer):
+    def __init__(self, d_model, nhead, dim_feedforward, dropout=0.1, activation="relu"):
+        super().__init__(d_model, nhead, dim_feedforward, dropout, activation)
 
-model = create_transformer_model(input_dim, hidden_dim, output_dim, num_heads, num_layers, dff)
+    def forward(self, tgt, tgt_mask=None, tgt_key_padding_mask=None):
+        tgt2 = self.self_attn(tgt, tgt, tgt, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)[0]
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
+        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+        return tgt
 
-pretrain(model, pretraining_data, epochs=20, batch_size=32)
+class ChessTransformer(nn.Module):
+    def __init__(self, d_model, nhead, num_layers, dim_feedforward, num_tokens=len(TOKENS), num_positions=64, move_output_size=4100):
+        super(ChessTransformer, self).__init__()
+        self.token_embedding = nn.Embedding(num_tokens, d_model)
+        self.positional_encoding = self.create_positional_encoding(num_positions, d_model)
 
-games_by_player = defaultdict(list)
-for game in filtered_games:
-    white_player = game.headers['White']
-    black_player = game.headers['Black']
-    games_by_player[white_player].append(game)
-    games_by_player[black_player].append(game)
+        decoder_layer = GPTDecoderLayer(d_model, nhead, dim_feedforward)
+        self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers)
 
-meta_train(model, filtered_players, epochs=100, k_shot_support=50, k_shot_query = 50, meta_lr=1e-3, inner_lr=1e-2)
+        self.final_attention = nn.MultiheadAttention(d_model, nhead)
+        self.output_layer = nn.Linear(d_model, move_output_size)
+
+    def create_positional_encoding(self, max_len, d_model):
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-torch.log(torch.tensor(64)) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        return nn.Parameter(pe, requires_grad=False)
+
+    def forward(self, x):
+        x = self.token_embedding(x) * torch.sqrt(torch.tensor(self.token_embedding.embedding_dim, dtype=torch.float))
+        x = x + self.positional_encoding[:x.size(0), :]
+        x = self.transformer_decoder(x)
+
+        # Pass the output of the transformer through the output layer
+        x = self.output_layer(x[-1])  # Use the last token's output
+        return x
+
+# Meta training function
+def maml_train(model, metatraining_set, inner_lr, outer_lr, inner_steps, num_episodes, num_support, num_query, device):
+    model.train()
+    outer_optimizer = optim.Adam(model.parameters(), lr=outer_lr)
+
+    for episode in range(num_episodes):
+        player = random.choice(list(metatraining_set.keys()))
+        data = random.sample(metatraining_set[player], num_support + num_query)
+
+        support_set = data[:num_support]
+        query_set = data[num_support:]
+
+        model_copy = copy.deepcopy(model)
+        model_copy.train()
+        inner_optimizer = optim.SGD(model_copy.parameters(), lr=inner_lr)
+
+        for _ in range(inner_steps):
+            for board_tokens, move_vector in support_set:
+                board_tokens = torch.tensor(board_tokens, dtype=torch.long, device=device).unsqueeze(0)
+                move_vector = torch.tensor(move_vector, dtype=torch.float, device=device).unsqueeze(0)
+
+                inner_optimizer.zero_grad()
+                output = model_copy(board_tokens)
+                loss = nn.BCEWithLogitsLoss()(output, move_vector)
+                loss.backward()
+                inner_optimizer.step()
+
+        outer_optimizer.zero_grad()
+        outer_loss = 0
+        for board_tokens, move_vector in query_set:
+            board_tokens = torch.tensor(board_tokens, dtype=torch.long, device=device).unsqueeze(0)
+            move_vector = torch.tensor(move_vector, dtype=torch.float, device=device).unsqueeze(0)
+
+            output = model_copy(board_tokens)
+            loss = nn.BCEWithLogitsLoss()(output, move_vector)
+            outer_loss += loss.item()
+
+        outer_loss /= num_query
+        outer_loss.backward()
+        outer_optimizer.step()
+
+        print(f"Episode {episode + 1}/{num_episodes}: Loss = {outer_loss.item()}")
+
+
+# Hyperparameters
+d_model = 256
+nhead = 8
+num_layers = 8
+dim_feedforward = 4*d_model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Initialize the model
+model = ChessTransformer(d_model, nhead, num_layers, dim_feedforward).to(device)
+
+# Load and process the dataset
+pgn_file = 'filtered_games.pgn'
+dataset = load_filtered_games(pgn_file)
+
+# Prepare the pretraining and metatraining sets
+pretraining_set = prepare_pretraining_set(dataset)
+metatraining_set = prepare_metatraining_set(dataset)
+
+# Pretrain the model
+pretrain_epochs = 10
+pretrain_batch_size = 32
+pretrain_lr = 1e-4
+
+pretrain_loader = DataLoader(pretraining_set, batch_size=pretrain_batch_size, shuffle=True)
+pretrain_optimizer = optim.Adam(model.parameters(), lr=pretrain_lr)
+
+for epoch in range(pretrain_epochs):
+    total_loss = 0
+    for i, (board_tokens, move_vector) in enumerate(pretrain_loader):
+        board_tokens = board_tokens.to(device)
+        move_vector = move_vector.to(device)
+
+        pretrain_optimizer.zero_grad()
+        output = model(board_tokens)
+        loss = nn.BCEWithLogitsLoss()(output, move_vector)
+        loss.backward()
+        pretrain_optimizer.step()
+
+        total_loss += loss.item()
+
+    print(f"Epoch {epoch + 1}/{pretrain_epochs}: Loss = {total_loss / len(pretrain_loader)}")
+
+# MAML train the model
+inner_lr = 1e-2
+outer_lr = 1e-4
+inner_steps = 5
+num_episodes = 500
+num_support = 10
+num_query = 10
+
+maml_train(model, metatraining_set, inner_lr, outer_lr, inner_steps, num_episodes, num_support, num_query, device)
+
+# Save the model
+torch.save(model.state_dict(), 'chess_transformer_' + str(d_model) + '_' + str(nhead) + '_' + str(num_layers) + '.pth')
 
