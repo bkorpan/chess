c650dfb1fe487f88dd96e9ec66bf93b4c5a1c22a
Ben Korpan <bkorpan@berkeley.edu>
2023-04-08 18:44:06 -0700

    Added batched self play

--- mcts.py
+++ mcts.py
@@ -31,6 +31,21 @@ def mcts(model, root, board, num_simulations, device, cpuct=1):
     children_with_max_visits = list(filter(lambda item: item[1].visits == max_visits, root.children.items()))
     return random.choice(children_with_max_visits)[0]
 
+def mcts_batched(model, roots, boards, num_simulations, batch_size, device, cpuct=1):
+    for _ in range(num_simulations):
+        nodes = []
+        for idx in range(batch_size):
+            nodes.append(select(roots[idx], boards[idx], cpuct))
+        values = expand_and_evaluate_batched(nodes, boards, batch_size, model, device)
+        for idx in range(batch_size):
+            backpropagate(nodes[idx], values[idx], boards[idx])
+    moves = []
+    for idx in range(batch_size):
+        max_visits = max(roots[idx].children.items(), key=lambda item: item[1].visits)[1].visits
+        children_with_max_visits = list(filter(lambda item: item[1].visits == max_visits, roots[idx].children.items()))
+        moves.append(random.choice(children_with_max_visits)[0])
+    return moves
+
 def select(node, board, cpuct):
     while node.expanded():
         move, node = max(node.children.items(), key=lambda item: uct(item[1], cpuct))
@@ -53,6 +68,29 @@ def expand_and_evaluate(node, board, model, device):
         node.children[move] = Node(parent=node, prior=policy[move_to_index(board, move)])
 
     return value[2] - value[0]
+
+def expand_and_evaluate_batched(nodes, boards, batch_size, model, device):
+    tokenized_boards = []
+    for idx in range(batch_size):
+        tokenized_boards.append(tokenize_board(boards[idx]))
+    boards_tensor = torch.tensor(tokenized_boards).to(device)
+    policy, value = model(boards_tensor)
+    policy = torch.nn.Softmax(dim=-1)(policy)
+    value = torch.nn.Softmax(dim=-1)(value)
+    policy = policy.cpu().detach().numpy()
+    value = value.cpu().detach().numpy()
+
+    values = []
+    for idx in range(batch_size):
+        legal_moves = list(boards[idx].legal_moves)
+        if not legal_moves:
+            values.append(-1 if board.is_checkmate() else 0)
+        else:
+            values.append(value[idx, 2] - value[idx, 0])
+            for move in legal_moves:
+                nodes[idx].children[move] = Node(parent=nodes[idx], prior=policy[idx, move_to_index(boards[idx], move)])
+
+    return values
 
 def backpropagate(node, value, board):
     while node is not None:
