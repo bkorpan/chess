3e0b63591cd1f5942d431e1dafe2b1b2d0f8e70d
Ben Korpan <benkorpan@gmail.com>
2024-04-28 22:15:01 -0700

    change default config to chess

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ num_devices = len(devices)
 
 
 class Config(BaseModel):
-    env_id: pgx.EnvId = "go_9x9"
+    env_id: pgx.EnvId = "chess"
     seed: int = 0
     max_num_iters: int = 400
     # network params
@@ -49,7 +49,7 @@ class Config(BaseModel):
     num_simulations: int = 32
     max_num_steps: int = 256
     # training params
-    training_batch_size: int = 4096
+    training_batch_size: int = 1024
     learning_rate: float = 0.001
     # eval params
     eval_interval: int = 5
@@ -228,7 +228,7 @@ def train(model, opt_state, data: Sample):
 
 @jax.pmap
 def evaluate(rng_key, my_model):
-    """A simplified evaluation by sampling. Only for debugging. 
+    """A simplified evaluation by sampling. Only for debugging.
     Please use MCTS and run tournaments for serious evaluation."""
     my_player = 0
     my_model_parmas, my_model_state = my_model
